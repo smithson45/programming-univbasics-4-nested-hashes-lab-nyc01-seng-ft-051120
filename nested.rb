11c75bf6e29ee012401e89c2f7e4dfe14b42bd55
Andre Carter <carstar2486@gmail.com>
2020-04-27 20:28:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,8 @@ def adding_matz
       }
     }
     
- return programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP","C"]}
+ programmer_hash += programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP","C"]}
+ 
  
 
 end
